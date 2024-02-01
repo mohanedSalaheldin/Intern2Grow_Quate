dccc76864d4123c085f1d6de7e9dc6e596618b54
mohanedSalaheldin <mohnsd1162002@gmail.com>
2024-02-01 21:48:23 +0200

    all done

--- lib/src/core/networks/network_checker.dart
+++ lib/src/core/networks/network_checker.dart
@@ -1,4 +1,5 @@
-import 'package:internet_connection_checker/internet_connection_checker.dart';
+// import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 
 abstract class NetworkInfo {
   Future<bool> get isConnected;
@@ -6,7 +7,37 @@ abstract class NetworkInfo {
 
 class NetworkInfoImpl implements NetworkInfo {
   // final hasConnection;
+  // @override
+  // Future<bool> get isConnected async =>
+  //     await InternetConnectionChecker().hasConnection;
   @override
-  Future<bool> get isConnected async =>
-      await InternetConnectionChecker().hasConnection;
+  Future<bool> get isConnected async {
+    final connectivityResult = await (Connectivity().checkConnectivity());
+    if (connectivityResult == ConnectivityResult.mobile ||
+        connectivityResult == ConnectivityResult.wifi) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
+
+/*
+import 'package:connectivity_plus/connectivity_plus.dart';
+
+final connectivityResult = await (Connectivity().checkConnectivity());
+if (connectivityResult == ConnectivityResult.mobile) {
+  // I am connected to a mobile network.
+} else if (connectivityResult == ConnectivityResult.wifi) {
+  // I am connected to a wifi network.
+} else if (connectivityResult == ConnectivityResult.ethernet) {
+  // I am connected to a ethernet network.
+} else if (connectivityResult == ConnectivityResult.vpn) {
+  // I am connected to a vpn network.
+  // Note for iOS and macOS:
+  // There is no separate network interface type for [vpn].
+  // It returns [other] on any device (also simulator)
+} 
+
+ */
+
--- lib/src/core/widgets/error_widget.dart
+++ lib/src/core/widgets/error_widget.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:hexcolor/hexcolor.dart';
-import 'package:quate_app/src/features/quate/presentation/widgets/quate_card.dart';
 
 class MyErrorWidget extends StatelessWidget {
   const MyErrorWidget({super.key, required this.error});
@@ -8,23 +6,21 @@ class MyErrorWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [HexColor('#5D13E7'), HexColor('#8249B5')],
-          begin: Alignment.topRight,
-          end: Alignment.bottomLeft,
-        ),
-      ),
-      child: Center(
-        child: QuateCard(
-          quateContent: error,
-          quateAuther: '',
-          child: Text(
-            'Reload Now',
-            style: Theme.of(context).textTheme.bodySmall,
-          ),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Image(
+              image: AssetImage('assets/images/offline.png'),
+            ),
+            Text(
+              'You\'re offline, Check your Internet',
+              style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    color: Colors.white,
+                  ),
+            ),
+          ],
         ),
       ),
     );
